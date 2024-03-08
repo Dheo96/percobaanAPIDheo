12e8112f3bb8fb835dc3e9e2bd88fd52d615d0e3
Farah Fairuz <farahfairuz014@gmail.com>
2024-03-08 19:43:53 +0700

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using percobaanAPIDheo.Models;
+
+namespace percobaanAPIDheo.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+    }
+}
